43b1616861525980fe6a497a37fe0d132e021a8a
camariana <camariana40@outlook.com>
2021-07-24 02:07:08 +0000

    add testing case on utils config

--- utils/config.js
+++ utils/config.js
@@ -7,10 +7,10 @@ const env = process.env.NODE_ENV
 switch(env) {
   case 'production':
     MONGODB_URI = process.env.LIVE_MONGODB_URI
-  case 'test':
-    MONGODB_URI = process.env.TEST_MONGODB_URI
-  default:
+  case 'development':
     MONGODB_URI = process.env.LOCAL_MONGODB_URI
+  default:
+    MONGODB_URI = process.env.TEST_MONGODB_URI
 }
 
 module.exports = {
