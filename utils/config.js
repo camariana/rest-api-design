48449158478a1883534456a24d58777c033ac31b
camariana <camariana40@outlook.com>
2021-07-27 21:54:29 +0000

    fixed the bug on not choosing the right db

--- utils/config.js
+++ utils/config.js
@@ -5,12 +5,14 @@ let MONGODB_URI = ''
 const env = process.env.NODE_ENV
 
 switch(env) {
+  case 'test':
+    MONGODB_URI = process.env.TEST_MONGODB_URI
+    break
   case 'production':
     MONGODB_URI = process.env.LIVE_MONGODB_URI
-  case 'development':
-    MONGODB_URI = process.env.LOCAL_MONGODB_URI
+    break
   default:
-    MONGODB_URI = process.env.TEST_MONGODB_URI
+    MONGODB_URI = process.env.LOCAL_MONGODB_URI
 }
 
 module.exports = {
